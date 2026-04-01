d9ff2dd3d7d3833651c2ed41568c95c50ff997b7
Sofiane949 <sofianekhourta@gmail.com>
2026-04-01 15:00:47 +0200

    essai resolution de bugs plugin wam video_babylonjs

--- src/plugins/video_babylonjs/src/index.tsx
+++ src/plugins/video_babylonjs/src/index.tsx
@@ -7,6 +7,8 @@ export default class VideoBabylonJS extends WebAudioModule<any> {
 	_node: any
 	_runner: BabylonRunner
 
+	get isVideoPlugin() { return true; }
+
 	async createAudioNode(initialState: any) {
 		const id = this.moduleId; 
 		
@@ -36,18 +38,14 @@ export default class VideoBabylonJS extends WebAudioModule<any> {
 			initialState 
 		} as any) as any;
 		
-		// Ajout de l'analyseur audio pour le FFT (comme dans le plugin de Tom Burns)
 		const analyser = this.audioContext.createAnalyser();
 		analyser.smoothingTimeConstant = 0.3;
 		node.analyser = analyser;
 		node.connect(analyser);
 
-		// ENREGISTREMENT DE L'EXTENSION VIDÉO
-		// C'est ce bloc qui permet à Sequencer Party de "voir" ton moteur 3D
 		if (window.WAMExtensions && window.WAMExtensions.video) {
-			window.WAMExtensions.video.setDelegate(node.instanceId, {
+			window.WAMExtensions.video.setDelegate((this as any).instanceId || node.instanceId, {
 				connectVideo: (options: any) => {
-					console.log("BABYLON: connectVideo reçu de l'hôte");
 					this._runner = new BabylonRunner(options);
 				},
 				config: () => {
@@ -55,17 +53,9 @@ export default class VideoBabylonJS extends WebAudioModule<any> {
 				},
 				render: (inputs: WebGLTexture[], currentTime: number): WebGLTexture[] => {
 					if (!this._runner) return inputs;
-					
-					// Extraction du FFT pour la réactivité
 					const fftArray = new Float32Array(node.analyser.frequencyBinCount);
 					node.analyser.getFloatFrequencyData(fftArray);
-					
-					// Appel au rendu Babylon
 					return this._runner.render(inputs, null, currentTime, {}, fftArray);
-				},
-				disconnectVideo: () => {
-					console.log("BABYLON: disconnectVideo");
-					if (this._runner) this._runner.destroy();
 				}
 			});
 		}
@@ -78,12 +68,12 @@ export default class VideoBabylonJS extends WebAudioModule<any> {
 		const div = document.createElement("div")
 		div.style.width = "100%";
 		div.style.height = "100%";
+		div.style.backgroundColor = "#111";
+		div.style.color = "#00ffcc";
 		div.style.display = "flex";
 		div.style.alignItems = "center";
 		div.style.justifyContent = "center";
-		div.style.backgroundColor = "#111";
-		div.style.color = "#00ffcc";
-		div.innerHTML = "<div style='text-align:center'><h2>BABYLON 3D</h2><p>Moteur spatialisé et connecté.</p></div>";
+		div.innerHTML = "<h2>BABYLON 3D</h2>";
 		return div
 	}
 }
